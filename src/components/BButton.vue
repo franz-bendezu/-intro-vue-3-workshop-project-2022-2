89f18085c631b5d35e37eaa1e18836f0b4e4f0e0
Franz Bendezu <franzant20@gmail.com>
2022-09-14 21:15:06 -0500

    Agregando botón

---
+++ src/components/BButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="button" :type="type" :class="buttonClasses">
+    <slot name="icon"> </slot>
+    <slot :count="count"> </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: () => "primary",
+    },
+    type: {
+      type: String,
+      default: () => "button",
+    },
+  },
+  data: () => ({
+    count: 0,
+  }),
+  computed: {
+    buttonClasses() {
+      const colorClass = "is-" + this.color;
+      return [colorClass];
+    },
+  },
+};
+</script>
